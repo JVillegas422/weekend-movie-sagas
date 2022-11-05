a9c3f719dec6474e170273267877f527b03f4def
Juan Villegas <jvillegas422@gmail.com>
2022-11-05 14:16:03 -0500

    Update genre router

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -2,9 +2,27 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool')
 
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {
   // Add query to get all genres
-  res.sendStatus(500)
+  const movieId = req.params.id;
+  const queryText = `
+    SELECT "genres"."name" 
+    FROM "genres"
+    JOIN
+      "movies_genres" ON "movies_genres"."genres_id" = "genres"."id"
+    JOIN
+      "movies" ON "movies_genres"."movie_id" = "movies"."id"
+    WHERE "movies"."id" = $1;
+  `;
+
+  pool.query(queryText, [movieId])
+    .then((result) => {
+      res.send(result.rows)
+    })
+    .catch((err) => {
+      console.log('Error in genre router', err);
+      res.sendStatus(500);
+    })
 });
 
 module.exports = router;
