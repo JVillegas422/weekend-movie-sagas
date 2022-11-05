3245a238cd75f0d8e9c28d337b63fb4ce34fc0c2
Juan Villegas <jvillegas422@gmail.com>
2022-11-05 13:28:02 -0500

    Update MovieDetails w/useSelectors & base setup to render to DOM

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import React from 'react';
 
 function MovieDetails() {
     console.log('in MovieDetails');
@@ -8,9 +9,29 @@ function MovieDetails() {
     const genres = useSelector(store => store.genres);
     const history = useHistory();
 
+    const handleClick = () => {
+        history.push('/');
+    }
+
     return (
         <>
+        <button onClick={handleClick}>Home Page</button>
             <h3>Movie Details:</h3>
+
+            <div key={movieDetails.id}>
+                <h3>{movieDetails.title}</h3>
+                <img
+                    src={movieDetails.poster} alt={movieDetails.title} />
+                <p>{movieDetails.description}</p>
+            </div>
+
+            <h3>Movie Genre</h3>
+            <p>{genres.name}</p>
+            {genres.map((genre, index) => {
+                return (
+                    <p key={index}>{genre.name}</p>
+                )
+            })}
         </>
     );
 }
